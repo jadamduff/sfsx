9e5a2c71cda3a998b2ef16d9968d016f04da83b9
Adam Duff <j.adam.duff@gmail.com>
2019-03-14 05:50:12 -0500

    Add open and close transition to TradingLog component

--- src/components/trading_log/TradingLog.js
+++ src/components/trading_log/TradingLog.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
 import * as styles from './styles';
 
 class TradingLog extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false
+    }
+  }
 
+  height = () => {
+    if (this.state.open) {
+      return (css`
+        height: 95%;
+        `)
+    } else {
+      return (css`
+        height: 65px;
+        `)
     }
   }
 
   render() {
+
     return (
-      <styles.Container>
+      <styles.Container css={css`
+          ${this.height()}
+        `}>
 
       </styles.Container>
     )
--- src/components/trading_log/styles.js
+++ src/components/trading_log/styles.js
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core'
 
 export const Container = styled.div`
   width: 450px;
-  height: 50px;
-  background-color: red;
+  background-color: #fff;
+  border-top: 2px solid #424242;
   position: absolute;
   bottom: 0;
   right: 25px;
   z-index: 3;
+  box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.2);
+  transition: height .2s;
+  transition-timing-function: ease;
+  cursor: pointer;
+  :hover {
+    box-shadow: 0 4px 5px 5px rgba(0,0,0,0.06), 0 1px 10px 2px rgba(0,0,0,0.04), 0 2px 4px -1px rgba(0,0,0,0.2);
+  }
 `
