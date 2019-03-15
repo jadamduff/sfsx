13f27e3bd9ad763bb4445a263aba978a11442731
Adam Duff <j.adam.duff@gmail.com>
2019-03-15 03:19:05 -0500

    Add scaffolding for createOrder and updateBook Redux actions

--- src/actions/orders.js
+++ src/actions/orders.js
@@ -5,5 +5,14 @@ export function createOrder(orderObj) {
     let finalizedOrderObj = orderObj;
     finalizedOrderObj.timestamp = timestamp();
     dispatch({type: 'CREATE_ORDER', payload: finalizedOrderObj});
+
+    // Call update book here, since creating an order will always update the book.
+
+  }
+}
+
+export function updateBook(orderObj) {
+  return (dispatch) => {
+    dispatch({type: "UPDATE_BOOK", payload: orderObj})
   }
 }
